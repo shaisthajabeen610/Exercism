dfbd9d25b47cd949c64125439b7e48c8ce300d10
shaisthajabeen610 <67311615+shaisthajabeen610@users.noreply.github.com>
2020-07-13 21:41:01 +0530

    Add files via upload

---
+++ scrabble-score/scrabble_score.go
@@ -0,0 +1,26 @@
+package scrabble
+import  "strings"
+//Score is a function used to find the score of given string
+//score returns the count for occurence of characters
+func Score(s string) (c int) {
+	s= strings.ToUpper(s)
+    c=0
+	maptype :=map[string]int{
+		 "AEIOULNRST":1,
+	      "DG":2,
+	     "BCMP":3,
+	     "FHVWY":4,
+	       "k":5,
+	     "JX":8,
+		 "QZ":10,
+	}
+	for _, str:=range s{
+	  for i, v:=range maptype{
+			 if strings.ContainsAny(i,string(str)){
+				 c+=v
+			 }
+		 }
+	}
+
+	return c
+}
